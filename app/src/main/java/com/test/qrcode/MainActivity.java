a6463501c163575b7afd4ec2dd3e6b58d7e7621f
zhongrui <zhongrui@021.com>
2020-01-09 21:05:19 +0800

    file

--- app/src/main/java/com/test/qrcode/MainActivity.java
+++ app/src/main/java/com/test/qrcode/MainActivity.java
@@ -1,13 +1,102 @@
 package com.test.qrcode;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Activity activity;
+    private Button bt;
+    boolean isOpen;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity=this;
         setContentView(R.layout.activity_main);
+        bt = findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isOpen = !isOpen;
+                openLight(activity,isOpen);
+                bt.setText(getTextForOpen(isOpen));
+            }
+        });
+        bt.setText(getTextForOpen(isOpen));
+    }
+
+    private void openLight(Context context, boolean isOpen) {
+        if(true){
+            if(camera==null){
+                camera = Camera.open();
+            }
+            Camera.Parameters parameters = camera.getParameters();
+            if(isOpen){
+                //打开闪光灯
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);//开启
+                camera.setParameters(parameters);
+            }else{
+                //关闭闪光灯
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);//关闭
+                camera.setParameters(parameters);
+            }
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                //获取CameraManager
+                CameraManager mCameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+                //获取当前手机所有摄像头设备ID
+                String[] ids = mCameraManager.getCameraIdList();
+                for (String id : ids) {
+                    CameraCharacteristics c = mCameraManager.getCameraCharacteristics(id);
+                    //查询该摄像头组件是否包含闪光灯
+                    Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                    Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                    if (flashAvailable != null && flashAvailable
+                            && lensFacing != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                        //打开或关闭手电筒
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            mCameraManager.setTorchMode(id, isOpen);
+                        }
+                    }
+                }
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        } else {
+            camera = Camera.open();
+            Camera.Parameters parameters = camera.getParameters();
+            if(isOpen){
+                //打开闪光灯
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);//开启
+                camera.setParameters(parameters);
+            }else{
+                //关闭闪光灯
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);//关闭
+                camera.setParameters(parameters);
+            }
+            return;
+        }
+
+    }
+
+    private String getTextForOpen(boolean isOpen) {
+        return isOpen ? "关闭闪光灯" : "打开闪光灯";
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        camera.release();
     }
 }
