efcde69dac2e64d24b3f74c451e704d1e84da196
windows10 <2380253499@qq.com>
2020-02-09 15:49:52 +0800

    file

--- QRCode/src/main/java/com/google/zxing/MultiFormatReader.java
+++ QRCode/src/main/java/com/google/zxing/MultiFormatReader.java
@@ -16,6 +16,21 @@
 
 package com.google.zxing;
 
+import com.google.zxing.aztec.AztecReader;
+import com.google.zxing.datamatrix.DataMatrixReader;
+import com.google.zxing.maxicode.MaxiCodeReader;
+import com.google.zxing.oned.CodaBarReader;
+import com.google.zxing.oned.Code128Reader;
+import com.google.zxing.oned.Code39Reader;
+import com.google.zxing.oned.Code93Reader;
+import com.google.zxing.oned.EAN13Reader;
+import com.google.zxing.oned.EAN8Reader;
+import com.google.zxing.oned.ITFReader;
+import com.google.zxing.oned.UPCAReader;
+import com.google.zxing.oned.UPCEReader;
+import com.google.zxing.oned.rss.RSS14Reader;
+import com.google.zxing.oned.rss.expanded.RSSExpandedReader;
+import com.google.zxing.pdf417.PDF417Reader;
 import com.google.zxing.qrcode.QRCodeReader;
 
 import java.util.ArrayList;
@@ -56,7 +71,10 @@ public final class MultiFormatReader implements Reader {
             if (this.codeFormat != null && !this.codeFormat.isEmpty()) {
                 for (int i = 0; i < codeFormat.size(); i++) {
                     String format = codeFormat.get(i);
-                    readers.add(getFormatMap().get(format));
+                    Reader reader = getFormatMap().get(format);
+                    if(reader!=null&&!readers.contains(reader)){
+                        readers.add(reader);
+                    }
                 }
             } else {
                 readers.add(new QRCodeReader());
@@ -70,7 +88,23 @@ public final class MultiFormatReader implements Reader {
     private Map<String, Reader> getFormatMap() {
         if (formatMap == null) {
             formatMap = new ConcurrentHashMap<>();
+
+            formatMap.put("AZTEC", new AztecReader());
+            formatMap.put("CODABAR", new CodaBarReader());
+            formatMap.put("CODE_39", new Code39Reader(false));
+            formatMap.put("CODE_93", new Code93Reader());
+            formatMap.put("CODE_128", new Code128Reader());
+            formatMap.put("DATA_MATRIX", new DataMatrixReader());
+            formatMap.put("EAN_8", new EAN8Reader());
+            formatMap.put("EAN_13", new EAN13Reader());
+            formatMap.put("ITF", new ITFReader());
+            formatMap.put("MAXICODE", new MaxiCodeReader());
+            formatMap.put("PDF_417", new PDF417Reader());
             formatMap.put("QR_CODE", new QRCodeReader());
+            formatMap.put("RSS_14", new RSS14Reader());
+            formatMap.put("RSS_EXPANDED", new RSSExpandedReader());
+            formatMap.put("UPC_A", new UPCAReader());
+            formatMap.put("UPC_E", new UPCEReader());
         }
         return formatMap;
     }
@@ -95,7 +129,9 @@ public final class MultiFormatReader implements Reader {
     public void reset() {
         if (readers != null) {
             for (Reader reader : readers) {
-                reader.reset();
+                if(reader!=null){
+                    reader.reset();
+                }
             }
         }
     }
