67851d2afac6260ee6166ad0eb1a20758a1e6815
zhongrui <2380253499@qq.com>
2020-02-12 18:06:40 +0800

    file

--- QRCode/src/main/java/com/github/qrcode/LoopThread.java
+++ QRCode/src/main/java/com/github/qrcode/LoopThread.java
@@ -234,5 +234,5 @@ public class LoopThread extends Thread {
     public void setCodeFormat(List<String> codeFormat) {
         this.codeFormat = codeFormat;
     }
-    //测试code
+    //测试code2222
 }
